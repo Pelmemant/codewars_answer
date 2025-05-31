5d29b6195260b5ee9f49f6a5a1560b0285c98b22
Pelmemant <basil.111@yandex.ru>
2025-05-31 19:32:09 +0700

    Update Array.diff.py

--- Array.diff.py
+++ Array.diff.py
@@ -1,4 +1,7 @@
-"""Implement a function that computes the difference between two lists. The function should remove all occurrences of elements from the first list (a) that are present in the second list (b). The order of elements in the first list should be preserved in the result."""
+"""Implement a function that computes the difference between two lists. 
+The function should remove all occurrences of elements from the first list (a) 
+that are present in the second list (b). The order of elements in the first 
+list should be preserved in the result."""
 
 def array_diff(a, b):
     for i in b:
